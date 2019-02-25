130b7b5ec39140c3a2b690406d0f3059eaceefc4
Clinton Dreisbach <clinton@dreisbach.us>
2019-02-25 15:40:33 -0500

    V1 of mystery word done

--- w3/examples/mystery-word/mystery_word.py
+++ w3/examples/mystery-word/mystery_word.py
@@ -1,7 +1,3 @@
-word_to_guess = "racecar"
-letters_guessed = []
-
-
 def word_guessed(word_to_guess, letters_guessed):
     """
     Given a word to guess and a list of the letters currently guessed,
@@ -14,6 +10,9 @@ def word_guessed(word_to_guess, letters_guessed):
 
 
 def is_letter_input(user_input):
+    """
+    Check to see if the input is one character long and is a letter.
+    """
     return len(user_input) == 1 and user_input.isalpha()
 
 
@@ -26,7 +25,28 @@ def get_letter_input():
     return user_input
 
 
+def get_display_word(word, letters_to_show):
+    """Given a word being guessed and the letters currently guessed,
+    display that word as a series of space-separated underscores (if the
+    current letter has not been guessed) or the current letter, uppercased.
+    """
+    output_chars = []
+    for letter in word:
+        if letter in letters_to_show:
+            output_chars.append(letter.upper())
+        else:
+            output_chars.append("_")
+    return " ".join(output_chars)
+
+
 if __name__ == "__main__":
+    word_to_guess = "racecar"
+    letters_guessed = []
+
     while not word_guessed(word_to_guess, letters_guessed):
+        print(get_display_word(word_to_guess, letters_guessed))
         letter_guessed = get_letter_input()
         letters_guessed.append(letter_guessed)
+
+    print(get_display_word(word_to_guess, letters_guessed))
+    print("You won!")
--- w3/examples/mystery-word/test_mystery_word.py
+++ w3/examples/mystery-word/test_mystery_word.py
@@ -1,4 +1,4 @@
-from mystery_word import word_guessed, is_letter_input
+from mystery_word import word_guessed, is_letter_input, get_display_word
 
 word_to_guess = 'racecar'
 
@@ -7,6 +7,7 @@ def test_word_guessed():
     assert not word_guessed(word_to_guess, [])
     assert not word_guessed(word_to_guess, ['r', 'c'])
     assert word_guessed(word_to_guess, ['r', 'e', 'a', 'c'])
+    assert word_guessed("cattail", ['c', 't', 'a', 'l', 'm', 'i'])
 
 
 def test_is_letter_input():
@@ -15,3 +16,11 @@ def test_is_letter_input():
     assert not is_letter_input("hello")
     assert not is_letter_input("")
     assert not is_letter_input("]")
+
+
+def test_get_display_word():
+    assert get_display_word("racecar", []) == "_ _ _ _ _ _ _"
+    assert get_display_word("racecar", ['a', 'c']) == "_ A C _ C A _"
+    assert get_display_word("racecar", ['a', 'c', 'r', 'e']) == "R A C E C A R"
+    assert get_display_word("racecar", ['h', 'i', 'j']) == "_ _ _ _ _ _ _"
+    assert get_display_word("racecar", ['h', 'i', 'j', 'c']) == "_ _ C _ C _ _"
